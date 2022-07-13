30c794f16654d4243681b6d1947c09e1cc16b70b
dragg54 <ajibolasadiq@yahoo.com>
2022-07-13 22:44:22 +0100

    added find render path

--- services/render.js
+++ services/render.js
@@ -1,3 +1,14 @@
+const axios = require("axios")
+
 exports.itemRoutes = (req, res) =>{
     res.render("items")
+}
+exports.allItemRoutes = (req, res) =>{
+    axios.get("http://localhost:3000/api/items")
+    .then(response=>{
+        const data = response.data
+        res.send(data)
+    }
+
+    )
 }
