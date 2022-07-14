f4c1001d673adb62f672aabd536fd49b988d7128
dragg54 <ajibolasadiq@yahoo.com>
2022-07-14 18:59:35 +0100

    --

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const app = express()
 app.set("view engine", "ejs")
 app.use(bodyparser.urlencoded({extended: true}))
 dotenv.config({path:"config.env"})
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 app.use("/assets", express.static(__dirname + "/assets"))
 app.use("/media", express.static("media"))
 app.use("/", require("./routes/routes") )
 app.use("/items", require("./routes/routes"))
 console.log("listening to port 3000")
-app.listen(3000)
+app.listen(PORT)
